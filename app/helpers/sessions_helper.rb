f4669c7cd8af61fae50f2e83eb38b14228bf592e
taaaaak <a3ae10511@gmail.com>
2019-03-17 18:05:41 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   ##### 現在ログインしているユーザーを返す (いる場合)
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
@@ -44,5 +48,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
